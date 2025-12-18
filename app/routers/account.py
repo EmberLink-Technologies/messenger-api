08c9bdf85c8af1005a0733ceff7abfb312edc0b5
Oleksandr Merkulov <oleksandrmerkuloff@gmail.com>
2025-12-18 13:04:08 +0200

    added endpoints  for auth and accounts

---
+++ app/routers/account.py
@@ -0,0 +1,21 @@
+from fastapi import APIRouter
+
+
+account_router = APIRouter(
+    prefix='/account'
+)
+
+
+@account_router.get('/myself')
+def get_my_account():
+    pass
+
+
+@account_router.patch('/myself')
+def update_my_account():
+    pass
+
+
+@account_router.get('/{user_id}')
+def get_user_account():
+    pass
---
+++ app/routers/auth.py
@@ -0,0 +1,21 @@
+from fastapi import APIRouter
+
+
+auth_router = APIRouter(
+    prefix='/auth',
+)
+
+
+@auth_router.post('/register')
+def register():
+    pass
+
+
+@auth_router.post('/login')
+def login():
+    pass
+
+
+@auth_router.post('/logout')
+def logout():
+    pass
